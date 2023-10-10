2efa41a205ead8def4d730edf3d21e9db503b330
Andrew <awoodcoc@indiana.edu>
2023-10-09 19:58:43 -0500

    completed studio 10/9 no bonus mission YET

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,35 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, message = "Password must be longer than 6 characters.")
     private String password;
+
+    @NotBlank(message = "Please retype your password to verify.")
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -39,5 +55,10 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getVerify() {
+        return verify;
+    }
+
 }
 
